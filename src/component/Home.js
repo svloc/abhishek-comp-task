7d7d282823728f10b1a1075f06d928494b4a4761
svloc <svloc39@gmail.com>
2021-10-23 21:20:17 +0530

    add Sticky icon

--- src/component/Home.js
+++ src/component/Home.js
@@ -6,6 +6,7 @@ export default function Home() {
         className="header-poster"
         style={{ backgroundImage: "url(../header-img.png)" }}
       ></div>
+
       <div className="container">
         <h1 className="heading">
           How to save those extra bucks while shopping 🏬
@@ -15,6 +16,16 @@ export default function Home() {
           <span style={{ margin: "0 0.75em" }}>•</span>
           <span>Oct 12, 2021</span>
         </div>
+      </div>
+      <div className="side-icon">
+        <i class="fal fa-arrow-to-top s-icon"></i>
+        <i class="fab fa-facebook-square s-icon"></i>
+        <i class="fab fa-twitter s-icon"></i>
+        <i class="fab fa-whatsapp s-icon"></i>
+        <i class="fab fa-flickr s-icon"></i>
+      </div>
+
+      <div className="container">
         <article>
           <p>
             📜 Make a shopping list - This is the most commonly heard advice but
@@ -49,9 +60,8 @@ export default function Home() {
 
       <div className="row subscribe">
         <div className="text-container">
-          <h2>Subscribe to our newsletter</h2> 
-          <h3>Get the latest posts delivered right to
-          your inbox.</h3>
+          <h2>Subscribe to our newsletter</h2>
+          <h3>Get the latest posts delivered right to your inbox.</h3>
         </div>
         <div className="input-container row">
           <input placeholder="Your email address" />
@@ -60,75 +70,81 @@ export default function Home() {
       </div>
 
       <div className="container fampay-container row">
-           <div><img src='../../logo.png'/></div>
-           <div className='icon-wrapper'>
-             <h2>FamPay Team</h2>
-           <div className='icon'>
-           <i class="fas fa-globe-americas"></i>
-           <i class="fab fa-facebook-square"></i>
-           <i class="fab fa-twitter"></i>
-           </div>
-           </div>
+        <div>
+          <img src="../../logo.png" />
+        </div>
+        <div className="icon-wrapper">
+          <h2>FamPay Team</h2>
+          <div className="icon">
+            <i class="fas fa-globe-americas"></i>
+            <i class="fab fa-facebook-square"></i>
+            <i class="fab fa-twitter"></i>
+          </div>
+        </div>
       </div>
 
       <div className="row card">
-         <div className='col-4'>
-           <img className='card-img' src='https://images.unsplash.com/photo-1496449903678-68ddcb189a24?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTc3M3wwfDF8c2VhcmNofDZ8fGZ1bnxlbnwwfHx8fDE2MjkxODMwMTM&ixlib=rb-1.2.1&q=80&w=2000'/>
-           <div className="card-body">
+        <div className="col-4">
+          <img
+            className="card-img"
+            src="https://images.unsplash.com/photo-1496449903678-68ddcb189a24?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTc3M3wwfDF8c2VhcmNofDZ8fGZ1bnxlbnwwfHx8fDE2MjkxODMwMTM&ixlib=rb-1.2.1&q=80&w=2000"
+          />
+          <div className="card-body">
             <p className="card-p">Culture</p>
-            <h2 className="card-text">13 Things to do when you're bored - Boredom busters for teens 🤳</h2>
+            <h2 className="card-text">
+              13 Things to do when you're bored - Boredom busters for teens 🤳
+            </h2>
             <div className="row">
-          <a>2 months ago</a>
-          <span style={{ margin: "0 0.75em" }}>•</span>
-          <span>4 min read</span>
+              <a>2 months ago</a>
+              <span style={{ margin: "0 0.75em" }}>•</span>
+              <span>4 min read</span>
+            </div>
+          </div>
         </div>
-           </div>
-         </div>
 
-         <div className='col-4'>
-           <img className='card-img' src='../../card-man.png'/>
-           <div className="card-body">
+        <div className="col-4">
+          <img className="card-img" src="../../card-man.png" />
+          <div className="card-body">
             <p className="card-p">FamSays</p>
-            <h2 className="card-text">Social Commerce - Making Money just through Social Media! 💸</h2>
+            <h2 className="card-text">
+              Social Commerce - Making Money just through Social Media! 💸
+            </h2>
             <div className="row">
-          <a>2 months ago</a>
-          <span style={{ margin: "0 0.75em" }}>•</span>
-          <span>3 min read</span>
+              <a>2 months ago</a>
+              <span style={{ margin: "0 0.75em" }}>•</span>
+              <span>3 min read</span>
+            </div>
+          </div>
         </div>
-           </div>
-         </div>
 
-
-         <div className='col-4'>
-           <img className='card-img' src='../../card-girl.png'/>
-           <div className="card-body">
+        <div className="col-4">
+          <img className="card-img" src="../../card-girl.png" />
+          <div className="card-body">
             <p className="card-p">LifeStyle</p>
-            <h2 className="card-text">It's time for teens to open shop on Instagram! 🛍️</h2>
-            <h2 style={{margin:'1em'}}></h2>
+            <h2 className="card-text">
+              It's time for teens to open shop on Instagram! 🛍️
+            </h2>
+            <h2 style={{ margin: "1em" }}></h2>
             <div className="row">
-          <a>6 months ago</a>
-          <span style={{ margin: "0 0.75em" }}>•</span>
-          <span>5 min read</span>
+              <a>6 months ago</a>
+              <span style={{ margin: "0 0.75em" }}>•</span>
+              <span>5 min read</span>
+            </div>
+          </div>
         </div>
-           </div>
-         </div>
-
-         
       </div>
-
 
       <footer className="row">
         <div className="footer-content">
-           <h3>UnRead by FamPay © 2021</h3>
-        <div className="footer-icon">
-        <i class="fab fa-instagram"></i>
-        <i class="fab fa-facebook-square"></i>
-        <i class="fab fa-linkedin"></i>
-           <i class="fab fa-twitter"></i>
-        </div>
+          <h3>UnRead by FamPay © 2021</h3>
+          <div className="footer-icon">
+            <i class="fab fa-instagram"></i>
+            <i class="fab fa-facebook-square"></i>
+            <i class="fab fa-linkedin"></i>
+            <i class="fab fa-twitter"></i>
+          </div>
         </div>
       </footer>
     </>
   );
 }
-
